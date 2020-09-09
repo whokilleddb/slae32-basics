4864820c7ada8f34b8c1c53e25a48e7d1824570b
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-09-09 07:40:10 -0400

    Add files via upload

---
+++ HelloWorld.asm
@@ -0,0 +1,19 @@
+global _start
+
+section .text
+
+_start:
+	mov eax, 0x4
+	mov ebx, 0x1
+	mov ecx, message
+	mov edx, mlen
+	int 0x80
+
+
+	mov eax, 0x1
+	mov ebx, 0x0
+	int 0x80
+
+section .data
+	message :db "Hello World!"
+	mlen equ $-message 
