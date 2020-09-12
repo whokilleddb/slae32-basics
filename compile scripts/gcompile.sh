4d4764243978ecc191fccc40b38ecf9c04729b02
Debjeet Banerjee <debjeetbanerjee06042001@gmail.com>
2020-09-12 08:25:20 +0000

    Create gcompile.sh

---
+++ compile scripts/gcompile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+str=$1
+str=${str%.asm}
+echo '[+] Assembling With Nasm'
+nasm -f elf32 -o $str.o $str.asm
+
+echo '[+] Linking'
+gcc -o $str $str.o
+
+echo [+] Created 32 Bit Executable : ${str}
+echo '[+] Done'
